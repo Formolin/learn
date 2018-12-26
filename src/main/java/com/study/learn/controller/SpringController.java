51db44ca37e7b4fdbd657b5c0edd55f31a470a4d
Formolin <liujiang.hrb@163.com>
2018-12-27 07:54:58 +0800

    日志

--- src/main/java/com/study/learn/controller/SpringController.java
+++ src/main/java/com/study/learn/controller/SpringController.java
@@ -12,19 +12,21 @@ public class SpringController {
     @RequestMapping("/hello")
     //经常使用下面的注解，说明返回的restful内容，不使用会进行跳转
 //    @ResponseBody
-    public String show(){
+    public String show() {
         return "adfadfasd";
     }
+
     @RequestMapping("/ok")
     //多个使用它，在这个类上使用@restController
 //    @ResponseBody
-    public String getCon(){
-        return "text";
+    public String getCon() {
+        return "44444aaaaa12312asasdfadsfdasdasdads313123";
     }
+
     @RequestMapping("/info/{msg}")//这个msg要传给形参，需要加注解@PathVariable
     //http://localhost:8080/info/restful!!!---访问时传递个值
     //支持Rest风格
-    public String fun(@PathVariable String msg){
-        return "haha"+msg;
+    public String fun(@PathVariable String msg) {
+        return "haha" + msg;
     }
 }
