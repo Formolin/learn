5262dafe6a670b17950bf6ac6502ffb85d38e160
Formolin <liujiang.hrb@163.com>
2018-12-26 22:33:58 +0800

    整合日志

--- src/main/java/com/study/learn/LearnApplication.java
+++ src/main/java/com/study/learn/LearnApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan("com.study.learn")
-@SpringBootApplication
+//@ComponentScan("com.study.learn")//不同包下扫描别的controller，进行实例化
+@SpringBootApplication(scanBasePackages = {"com.study.learn"})
 public class LearnApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/study/learn/controller/SpringController.java
+++ src/main/java/com/study/learn/controller/SpringController.java
@@ -1,14 +1,30 @@
 package com.study.learn.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+//@Controller
+@RestController//如果每个功能返回的都是restful内容，那么可以使用该注解，可以省去responsebody注解
 public class SpringController {
     @RequestMapping("/hello")
-    @ResponseBody
+    //经常使用下面的注解，说明返回的restful内容，不使用会进行跳转
+//    @ResponseBody
     public String show(){
-        return "hello";
+        return "adfadfasd";
+    }
+    @RequestMapping("/ok")
+    //多个使用它，在这个类上使用@restController
+//    @ResponseBody
+    public String getCon(){
+        return "text";
+    }
+    @RequestMapping("/info/{msg}")//这个msg要传给形参，需要加注解@PathVariable
+    //http://localhost:8080/info/restful!!!---访问时传递个值
+    //支持Rest风格
+    public String fun(@PathVariable String msg){
+        return "haha"+msg;
     }
 }
